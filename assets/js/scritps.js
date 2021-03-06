1f772ffb1c9c4220170ea5f81af69f1345c17529
matheus souza <ruivorobado@gmail.com>
2021-03-06 03:31:52 -0300

    funcao para o tema voltar ao normal

--- assets/js/scritps.js
+++ assets/js/scritps.js
@@ -3,12 +3,20 @@ window.addEventListener('click', e =>{
   el = e.target
   if(el.classList.contains('circle') ===  true){
 
-    if(el.style.left == 0){
-      el.style.right= 0
+    if(el.style.left === '0px'){
+      el.style.right= '0px'
       mudaTema()
       return
+    }else if(el.style.right === '0px')
+    {
+      el.style.left = '0px'
+      voltaTema()
+      return
+    }else{
+      el.style.right = '0px'
+      mudaTema()
     }
-    el.style.right = 0
+    
   }
 })
 
@@ -18,4 +26,11 @@ function mudaTema(){
   header.style.background = '#8F2D56'
   header.style.color = 'white'
   const ciculo = document.querySelector('.circle').style.background = '#73D2DE'
+}
+function voltaTema(){
+  const corpo = document.querySelector('body').style.background = 'white'
+  const header = document.querySelector('header#header')
+  header.style.background = '#7EE8FA'
+  header.style.color = 'black'
+  const ciculo = document.querySelector('.circle').style.background = '#80FF72'
 }
